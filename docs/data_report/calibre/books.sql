9d676099af828d397610d8dff1c563f6d2017209
wilsonify <tom.andrew.wilson@gmail.com>
2025-04-13 09:28:54 -0400

    books.sql

---
+++ docs/data_report/calibre/books.sql
@@ -0,0 +1,21 @@
+-- books definition
+
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title     TEXT NOT NULL DEFAULT 'Unknown' COLLATE NOCASE,
+    sort      TEXT COLLATE NOCASE,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    pubdate   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    series_index REAL NOT NULL DEFAULT 1.0,
+    author_sort TEXT COLLATE NOCASE,
+    isbn TEXT DEFAULT "" COLLATE NOCASE,
+    lccn TEXT DEFAULT "" COLLATE NOCASE,
+    path TEXT NOT NULL DEFAULT "",
+    flags INTEGER NOT NULL DEFAULT 1,
+    uuid TEXT,
+    has_cover BOOL DEFAULT 0,
+    last_modified TIMESTAMP NOT NULL DEFAULT "2000-01-01 00:00:00+00:00"
+    );
+
+CREATE INDEX authors_idx ON books (author_sort COLLATE NOCASE);
+CREATE INDEX books_idx ON books (sort COLLATE NOCASE);
